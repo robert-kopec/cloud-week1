4cb67a901f0da7486336f0b81e3e9b83ff132faa
Robert Kopec <robert.kopec@ivanti.com>
2021-02-06 13:38:09 +0000

    added tests

--- test.js
+++ test.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
     res.send('Hello Virtual Machine!')
 })
 
+app.get('/servicea', (req, res) => {
+    res.send('Get service A!')
+})
+
+app.get('/serviceb', (req, res) => {
+    res.send('Get service B!')
+})
+
+app.post('/', (req, res) => {
+    res.send('Get a post request!')
+})
+
+app.put('/', (req, res) => {
+    res.send('Get a put request!')
+})
+
+app.delete('/', (req, res) => {
+    res.send('Get a delete request!')
+})
+
 app.listen(port, () => {
     console.log(`Express Application listening at port 3000`)
 })
